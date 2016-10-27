0548e367090483799d735d5144de1331467bbc4a
Romain Reuillon <romain.reuillon@iscpif.fr>
2016-10-27 18:19:56 +0200

    First ramdom dsl

---
+++ build.sbt
@@ -0,0 +1,44 @@
+
+
+organization := "fr.iscpif"
+name := "freedsl"
+publishArtifact := false
+
+lazy val root = (project in file(".")).
+  aggregate(
+    random
+  ) settings(
+    publishArtifact := false
+  )
+
+def settings = Seq (
+  scalaVersion := "2.11.8",
+  scalaOrganization := "org.typelevel",
+  organization := "fr.iscpif.freedsl",
+  crossScalaVersions := Seq("2.11.8"),
+  libraryDependencies += "com.projectseptember" %% "freek" % "0.6.0",
+  libraryDependencies += "org.spire-math" %% "kind-projector" % "0.7.1",
+  publishTo := {
+    val nexus = "https://oss.sonatype.org/"
+    if (isSnapshot.value) Some("snapshots" at nexus + "content/repositories/snapshots")
+    else Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+  },
+  publishMavenStyle := true,
+  publishArtifact in Test := false,
+  pomIncludeRepository := { _ => false },
+  licenses := Seq("Affero GPLv3" -> url("http://www.gnu.org/licenses/")),
+  homepage := Some(url("https://github.com/ISCPIF/freedsl")),
+  scmInfo := Some(ScmInfo(url("https://github.com/ISCPIF/freedsl.git"), "scm:git:git@github.com:ISCPIF/freedsl.git")),
+  pomExtra := (
+    <developers>
+      <developer>
+        <id>romainreuillon</id>
+        <name>Romain Reuillon</name>
+      </developer>
+    </developers>
+  )
+)
+
+lazy val random  = Project(id = "random", base = file("random"), settings = settings)
+
+
---
+++ random/src/main/scala/freedsl/random/package.scala
@@ -0,0 +1,50 @@
+/**
+  * Created by Romain Reuillon on 27/10/16.
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Affero General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  *
+  */
+package freedsl
+
+import cats._
+import cats.implicits._
+import freek._
+
+package object random {
+
+  trait RNG[M[_]] {
+    def nextDouble: M[Double]
+    def nextInt(n: Int): M[Int]
+  }
+
+  object RNG {
+    sealed trait DSL[A]
+    final case object NextDouble extends DSL[Double]
+    final case class NextInt(n: Int) extends DSL[Int]
+
+    type PRG = DSL :|: NilDSL
+    val PRG = DSL.Make[PRG]
+
+
+    def interpreter(random: util.Random) = new (DSL ~> Id) {
+      def apply[A](a: DSL[A]) = a match {
+        case NextDouble => random.nextDouble
+        case NextInt(n) => random.nextInt(n)
+      }
+    }
+
+    def interpreter(seed: Long): DSL ~> Id = interpreter(new util.Random(seed))
+  }
+
+}
