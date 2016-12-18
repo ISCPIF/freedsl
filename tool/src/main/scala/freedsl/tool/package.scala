cda6c75e71403952645d461767f5ae9bd76341f6
Romain Reuillon <romain.reuillon@iscpif.fr>
2016-12-18 18:36:14 +0100

    Add until with kleisli end condition.

--- tool/src/main/scala/freedsl/tool/package.scala
+++ tool/src/main/scala/freedsl/tool/package.scala
@@ -1,12 +1,15 @@
 package freedsl
 
 import cats._
+import cats.data.Kleisli
 import cats.implicits._
 
 package object tool {
 
   implicit class MonadDecorator[M[_]: Monad, A](m: M[A]) {
-    def until(end: M[Boolean]): M[A] = {
+    def until(end: M[Boolean]): M[A] = until(Kleisli[M, A, Boolean](_ => end))
+
+    def until(end: Kleisli[M, A, Boolean]): M[A] = {
       def stop(a: A): Either[Unit, A] = Right(a)
       val continue: Either[Unit, A] = Left(Unit)
 
@@ -14,7 +17,7 @@ package object tool {
         val comp =
           for {
             a <- m
-            b <- end
+            b <- end.run(a)
           } yield (b, a)
 
         comp.map { case (e, a) => (if(e) stop(a) else continue) }
@@ -22,6 +25,7 @@ package object tool {
 
       loop
     }
+
   }
 
   def modifier[F[_] : Monad, T](get: F[T], set: T => F[Unit]) = new {
