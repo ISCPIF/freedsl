a5de2c76d890e375559d9dc0c41cc4c06bec7c6b
Romain Reuillon <romain.reuillon@iscpif.fr>
2017-01-13 14:39:37 +0100

    Rename the error.

--- dsls/io/src/main/scala/freedsl/io/IO.scala
+++ dsls/io/src/main/scala/freedsl/io/IO.scala
@@ -8,15 +8,15 @@ object IO {
 
   def interpreter = new Interpreter[Id] {
     def interpret[_] = {
-      case run(f) => util.Try(f()).toEither.leftMap(t => IOException(t))
-      case exception(t) => Left(IOException(t))
-      case exceptionOrResult(e) => e.leftMap(t => IOException(t))
-      case errorMessage(m) => Left(IOException(new RuntimeException(m)))
-      case errorMessageOrResult(e) => e.leftMap(m => IOException(new RuntimeException(m)))
+      case run(f) => util.Try(f()).toEither.leftMap(t => IOError(t))
+      case exception(t) => Left(IOError(t))
+      case exceptionOrResult(e) => e.leftMap(t => IOError(t))
+      case errorMessage(m) => Left(IOError(new RuntimeException(m)))
+      case errorMessageOrResult(e) => e.leftMap(m => IOError(new RuntimeException(m)))
     }
   }
 
-  case class IOException(t: Throwable) extends Error
+  case class IOError(t: Throwable) extends Error
 
 }
 
