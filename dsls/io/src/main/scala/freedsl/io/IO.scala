c416b6eb80f8bd88ad3092171af20c472dbd2163
Romain Reuillon <romain.reuillon@iscpif.fr>
2017-01-13 14:36:48 +0100

    Add error handeling function to IO.

--- dsls/io/src/main/scala/freedsl/io/IO.scala
+++ dsls/io/src/main/scala/freedsl/io/IO.scala
@@ -1,26 +1,31 @@
 package freedsl.io
 
 import cats._
+import cats.syntax.all._
 import freedsl.dsl._
-
 
 object IO {
 
   def interpreter = new Interpreter[Id] {
     def interpret[_] = {
-      case run(f) =>
-        util.Try(f()) match {
-          case util.Success(r) => Right(r)
-          case util.Failure(t) => Left(IOError(t))
-        }
+      case run(f) => util.Try(f()).toEither.leftMap(t => IOException(t))
+      case exception(t) => Left(IOException(t))
+      case exceptionOrResult(e) => e.leftMap(t => IOException(t))
+      case errorMessage(m) => Left(IOError(m))
+      case errorMessageOrResult(e) => e.leftMap(m => IOError(m))
     }
   }
 
-  case class IOError(t: Throwable) extends Error
+  case class IOException(t: Throwable) extends Error
+  case class IOError(message: String) extends Error
 
 }
 
 @dsl trait IO[M[_]] {
   def run[A](f: () => A): M[A]
   def apply[A](f: => A) = run(() => f)
+  def exception(t: Throwable): M[Unit]
+  def exceptionOrResult[A](either: Either[Throwable, A]): M[A]
+  def errorMessage(e: String): M[Unit]
+  def errorMessageOrResult[A](either: Either[String, A]): M[A]
 }
