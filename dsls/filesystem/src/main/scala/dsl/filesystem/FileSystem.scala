a070b666a2dddef629cb1f9938184e431d1b0c75
Romain Reuillon <romain.reuillon@iscpif.fr>
2017-01-25 14:13:41 +0100

    Add readStream method.

--- dsls/filesystem/src/main/scala/dsl/filesystem/FileSystem.scala
+++ dsls/filesystem/src/main/scala/dsl/filesystem/FileSystem.scala
@@ -1,6 +1,10 @@
 package dsl.filesystem
 
+import java.io._
 import freedsl.dsl._
+import util.{Failure, Success, Try}
+import cats._
+import cats.syntax.all._
 
 object FileSystem {
 
@@ -8,19 +12,31 @@ object FileSystem {
     implicit def stringToPath(path: String) = Path(new java.io.File(path))
     implicit def fileToPath(file: java.io.File) = Path(file)
   }
-  case class Path(path: java.io.File) extends AnyVal
+  case class Path(path: java.io.File) extends AnyVal {
+    override def toString = path.getPath
+  }
 
   def interpreter = new Interpreter[Id] {
     def interpret[_] = {
-      case list(p) => Right(p.path.listFiles.toVector.map(Path.apply))
+      case list(p) =>
+        Try(p.path.listFiles.toVector.map(Path.apply)).toEither.leftMap(e => FileError(s"Error listing directory $p", e))
+      case readStream(path, f) =>
+        Try {
+          val is = new BufferedInputStream(new FileInputStream(path.path))
+          try f(is)
+          finally is.close
+        }.toEither.leftMap(e => FileError(s"Error reading file $path", e))
     }
   }
+
+  case class FileError(message: String, cause: Throwable) extends Exception(message, cause) with Error
 
 }
 
 
 @dsl trait FileSystem[M[_]] {
   def list(path: FileSystem.Path): M[Vector[FileSystem.Path]]
+  def readStream[T](path: FileSystem.Path, f: InputStream => T): M[T]
 }
 
 
