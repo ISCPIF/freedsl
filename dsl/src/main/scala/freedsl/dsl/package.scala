8f0c1cf9b7dba9a1b29c6fa02befcfc985891a1b
Romain Reuillon <romain.reuillon@iscpif.fr>
2017-09-03 11:39:14 +0200

    Add implicit for Free.

--- dsl/src/main/scala/freedsl/dsl/package.scala
+++ dsl/src/main/scala/freedsl/dsl/package.scala
@@ -1,7 +1,10 @@
 package freedsl
 
+import cats.free.Free
+
 import scala.util.Try
 import cats.implicits._
+import cats.~>
 
 package object dsl {
   type DSL[T] = cats.free.Free[Try, T]
@@ -10,4 +13,7 @@ package object dsl {
     def eval = tryEval.get
     def tryEval = f.runTailRec
   }
+
+  implicit def freestyleTaglessLiftFree[F[_]] = freestyle.tagless.freestyleTaglessLiftFree[F]
+
 }
