a08f38f25264aa7cb64d461fd298bd5d10370f84
Romain Reuillon <romain.reuillon@iscpif.fr>
2017-11-06 14:02:53 +0100

    Add current time to system dsl.

--- system/src/main/scala/freedsl/system/System.scala
+++ system/src/main/scala/freedsl/system/System.scala
@@ -7,9 +7,11 @@ import squants._
 @tagless trait System {
   def randomUUID(): FS[UUID]
   def sleep(duration: Time): FS[Unit]
+  def currentTime(): FS[Long]
 }
 
 case class SystemInterpreter() extends System.Handler[freedsl.dsl.Evaluated] {
   def randomUUID() = freedsl.dsl.guard(UUID.randomUUID())
   def sleep(d: Time) = freedsl.dsl.guard(Thread.sleep(d.millis))
+  def currentTime() = freedsl.dsl.guard(java.lang.System.currentTimeMillis())
 }
